86b75c514ef9f850192908cb528ca17dfad7919c
LostKey09 <dev.aldrinecristobal@gmail.com>
2026-07-26 17:34:33 +0800

    [FEAT] Score and Breakdown in Win scene

--- Assets/Scripts/FinalScore.cs
+++ Assets/Scripts/FinalScore.cs
@@ -1,18 +1,46 @@
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class FinalScore : MonoBehaviour
 {
     [SerializeField] 
-    private Text scoreText; 
+    private TMP_Text scoreText;
+    [SerializeField] 
+    private TMP_Text breakdownText;
+    
     [SerializeField] 
     private Score score;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (scoreText != null) scoreText.text = ""; 
+        if (scoreText != null)
+        {
+            scoreText.text = $"{starScore()}";
+        } 
 
-        scoreText.text = $"SCORE: {score.playerScore}";
+        breakdownText.text = 
+        $"Perfect: {score.perfectCount}\n"+
+        $"Great: {score.greatCount}\n"+
+        $"Safe: {score.safeCount}\n"+
+        $"Miss: {score.missCount}\n"+
+        $"Fail: {score.failCount}";
+
+        Debug.Log($"{score.playerScore}");
+    }
+
+    string starScore()
+    {
+        string starString = "⭐";
+        int points = score.playerScore;
+        if (points >= 500)
+        {
+            starString += "⭐";
+        }
+        if (points >= 1000)
+        {
+            starString += "⭐";
+        }
+        return starString;
     }
 
     // Update is called once per frame
